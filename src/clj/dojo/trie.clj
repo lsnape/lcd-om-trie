a4e1c8bac7e3f9f7053806619d9046c858fe87bf
Luke Snape <ldjsnape@gmail.com>
2014-01-15 18:20:08 +0000

    Removing old comments.

--- src/clj/dojo/trie.clj
+++ src/clj/dojo/trie.clj
@@ -10,21 +10,12 @@
        slurp
        split-lines)))
 
-; helper method !!! might be suitable somewhere ese
 (defn in?
   "true if seq contains elem"
   [seq elem]
   (some #(= elem %) seq))
 
-; from SO post http://stackoverflow.com/questions/1452680/clojure-how-to-generate-a-trie
-
-; to understand what add-to-trie is doing, we first have to understand assoc-in
-; assoc-in takes 3 args: a map, an arbitrary sequence of KEYS, and a value
-;
-;   >> (assoc-in {} ["a" :a 1] 42) ; returns {"a" {:a {1 42}}}
-;
-; in our case, we treat the string as a sequence of chars
-; the terminal key/value pair is used to denote the end of a word in the trie
+; extended from SO post http://stackoverflow.com/questions/1452680/clojure-how-to-generate-a-trie
 
 (defn add-to-trie [trie elem]
   "given a trie (hashmap), return a new hashmap with elem added"
